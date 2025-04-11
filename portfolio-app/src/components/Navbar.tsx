3302314c03cd9a5ec1d0f1b33509a2bee1b6bb51
afsar-ali-380 <afsar380535@gmail.com>
2025-04-11 23:14:11 +0530

    first commit

---
+++ portfolio-app/src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { Dispatch, SetStateAction } from "react";
+
+export const Navbar = ({
+  menuOpen,
+  setMenuOpen,
+}: {
+  menuOpen: Boolean;
+  setMenuOpen: Dispatch<SetStateAction<Boolean>>;
+}) => {
+  return (
+    <div className="fixed top-0 z-50 w-full bg-[rgba(10,10,10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-xl">
+      <div className="max-w-5xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          <a href="#home" className="font-mono text-xl font-bold text-white">
+            afsar<span className="text-blue-500">.tech</span>
+          </a>
+
+          <div
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            &#9776;
+          </div>
+
+          <div className="hidden md:flex items-center space-x-8">
+            <a
+              href="#home"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              About
+            </a>
+            <a
+              href="#projects"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ portfolio-app/src/components/sections/Contact.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { RevealOnScroll } from "../RevealOnScroll";
+import emailjs from "emailjs-com";
+
+export const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const SERVICE_ID = "service_dgzp2cw";
+  const TEMPLATE_ID = "template_r8mzawf";
+  const PUBLIC_KEY = "BItOYWao2hVWjWO26";
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, form, PUBLIC_KEY)
+      .then(() => {
+        alert("Message sent!");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch(() => alert("Something went wrong. Please try again."));
+  };
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="px-4 w-150">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            Get In Touch
+          </h2>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="relative">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Name..."
+              />
+            </div>
+            <div className="relative">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div className="relative">
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                required
+                value={formData.message}
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
+                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Your Message..."
+              ></textarea>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500/75 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,249,0.4)] cursor-pointer"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
